6610d42bbc0fa1bed8ff5fc762953cdd36a4c07a
tushharsiingh19 <tushararanya2004@gmail.com>
2026-06-10 01:18:08 +0530

    api naming

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ,
+  baseURL: 'https://moodtune-5fws.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 30000,
 })
